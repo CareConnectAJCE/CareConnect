925681a81b4bf5ad61fe9449a734eb833bbd1ec4
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2024-01-24 16:53:42 +0530

    Add new URLs for user editing and appointment management

--- home/urls.py
+++ home/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path("callback", views.callback, name="callback"),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('edit_user', views.edit_user, name='edit_user'),
     path('doctor/', views.doctor_view, name='doctor'),
     path('patient/', views.patient_view, name='patient'),
+    path('appointment/visited/', views.appointment_visited, name='mark_visited'),
+    path('appointment/deleted/', views.appointment_deleted, name='mark_deleted'),
     path('chat/', views.chatbot_landing, name='chat'),
     path('response/', views.get_bot_response, name='bot_response')
 ]
